60047606a479727260758921a34623c662a38a31
Pushpraj mishra <pushparajsonu1@gmail.com>
2024-05-04 20:27:25 +0530

    installed ef core

---
+++ WebApplication1/WebApplication1/Data/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DBOperations.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base (options)
+        {
+            
+
+        }
+    }
+}
